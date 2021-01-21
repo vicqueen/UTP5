830693c1df3ce9de7c21bac61e9cf74e515ca1d2
Wiktoria Prusik <wiktoria@prusik.info>
2021-01-21 16:30:24 +0100

    feat: use reflection to invoke method from model

--- UTP5_PW_S19058/src/zad1/Controller.java
+++ UTP5_PW_S19058/src/zad1/Controller.java
@@ -1,32 +1,54 @@
 package zad1;
 
+import zad1.models.Bind;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class Controller {
 
     private String modelName;
+    private String data;
 
-    Controller(String modelName) {
+    public Controller(String modelName) {
         this.modelName = modelName;
     }
 
-    Controller readDataFrom(String fname) {
+    public Controller readDataFrom(String fname) {
         //wczytuje dane do obliczeń z pliku o nazwie fname
         return this;
     }
 
-    Controller runModel() {
+    public Controller runModel() {
         //uruchamia obliczenia modelowe
+        try {
+            Class modelClass = Class.forName("zad1.models." + modelName);
+            for (Field field : modelClass.getDeclaredFields()) {
+                Bind bind = field.getAnnotation(Bind.class);
+                if (bind != null) {
+                    field.setAccessible(true);
+                    Object childClass = modelClass.newInstance();
+                    field.set(childClass, 5);
+                    modelClass.getDeclaredMethods()[0].invoke(childClass);
+                }
+            }
+
+        } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
         return this;
     }
 
-    Controller runScriptFromFile(String fname) {
+    public Controller runScriptFromFile(String fname) {
         return this;
     }
 
-    Controller runScript(String script) {
+    public Controller runScript(String script) {
         return this;
     }
 
-    String getResultsAsTsv() {
+    public String getResultsAsTsv() {
         //zwraca wyniki obliczeń (wszystkie zmienne z modelu oraz zmienne utworzone w skryptach) w postaci napisu,
         // którego kolejne wiersze zawierają nazwę zmiennej i jej wartosci,
         // rozdzielone znakami tabulacji.

6634649ffddeb07e0b2c91779f3f6eac7fc83a8a
Wiktoria Prusik <wiktoria@prusik.info>
2021-01-21 19:59:45 +0100

    feat: read data from file and pass to model fields using reflection

--- UTP5_PW_S19058/src/zad1/Controller.java
+++ UTP5_PW_S19058/src/zad1/Controller.java
@@ -2,25 +2,49 @@ package zad1;
 
 import zad1.models.Bind;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Controller {
 
     private String modelName;
-    private String data;
+    private HashMap<String, Object> fieldsValues;
 
     public Controller(String modelName) {
         this.modelName = modelName;
+        fieldsValues = new HashMap<String, Object>();
     }
 
     public Controller readDataFrom(String fname) {
-        //wczytuje dane do obliczeń z pliku o nazwie fname
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fname), StandardCharsets.UTF_8);
+
+            for (int i = 0; i < lines.size(); i++) {
+                String[] lineSplit = lines.get(i).split("\t");
+                Object[] fieldValue = lineSplit[1].split(" ");
+
+                if (i == 0) {
+                    fieldsValues.put("LL", fieldValue.length );
+                } else {
+                    double[] values = Arrays.stream(fieldValue).mapToDouble(s -> Double.parseDouble((String) s)).toArray();
+                    fieldsValues.put(lineSplit[0], values);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return this;
     }
 
     public Controller runModel() {
-        //uruchamia obliczenia modelowe
         try {
             Class modelClass = Class.forName("zad1.models." + modelName);
             for (Field field : modelClass.getDeclaredFields()) {
@@ -28,11 +52,10 @@ public class Controller {
                 if (bind != null) {
                     field.setAccessible(true);
                     Object childClass = modelClass.newInstance();
-                    field.set(childClass, 5);
+                    field.set(childClass, fieldsValues.get(field.getName()));
                     modelClass.getDeclaredMethods()[0].invoke(childClass);
                 }
             }
-
         } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
             e.printStackTrace();
         }
